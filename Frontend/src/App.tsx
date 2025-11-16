08cb603b760cf18e6a7470e7aaea95b4bca6c40e
Bhupen05 <chauhanbhupen66@gmail.com>
2025-11-16 20:25:35 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json for frontend dependencies and scripts
    - Included Vite logo SVG in public directory
    - Created App.css and index.css with Tailwind CSS imports
    - Developed main App component with Tailwind styling
    - Added React logo SVG in assets directory
    - Configured TypeScript with tsconfig files for app and node
    - Set up Vite configuration for React and Tailwind CSS
    - Created batch scripts for installing backend and frontend dependencies

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold text-zinc-950 underline w-100% h-screen content-center text-center bg-linear-to-r from-red-500 via-indigo-600 to-blue-500">
+    Start you coding from here 
+  </h1>
+    </>
+  )
+}
+
+export default App
